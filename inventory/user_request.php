22760ec2021d8fcf0f97cfd27f9ab9d48cd5674c
Capstone Team MIS-GMIS 2025 <misgmis2025@users.noreply.github.com>
2025-11-18 22:30:29 +0800

    fix tableasd

--- inventory/user_request.php
+++ inventory/user_request.php
@@ -3075,7 +3075,7 @@ if (!empty($my_requests)) {
     })();
   </script>
   
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  
   <script>
   (function(){
     const readerId = 'urQrReader';
