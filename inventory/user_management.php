6ece4a0ba912256089d1629548526c3deeaa057c
Capstone Team MIS-GMIS 2025 <misgmis2025@users.noreply.github.com>
2025-12-10 22:39:23 +0800

    mobile123123123123123123123h123123612312312312312312312312312312312312382123123123123123123123123123xdd123k

--- inventory/user_management.php
+++ inventory/user_management.php
@@ -1007,7 +1007,7 @@ try {
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Physical Verification Decision</h5>
+            <h5 class="modal-title">Confirmation</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
